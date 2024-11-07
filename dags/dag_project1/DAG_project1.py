c2e11489be798015cf03def0316595eae69754a0
mag1cfrog <harrywong2017@gmail.com>
2024-11-07 09:04:23 -0800

    Refactor configuration settings into a new config.py file for better organization in DAG_project1.py

--- dags/dag_project1/DAG_project1.py
+++ dags/dag_project1/DAG_project1.py
@@ -3,18 +3,11 @@ from datetime import datetime
 
 from airflow import DAG
 
+from config import SNOWFLAKE_CONN_ID, SNOWFLAKE_STAGE, SNOWFLAKE_SCHEMA
 from helper import create_prestage_table, load_data_to_snowflake
 
 
-SNOWFLAKE_CONN_ID = 'snowflake_conn'
-SNOWFLAKE_DATABASE = 'AIRFLOW1007'
-SNOWFLAKE_SCHEMA = 'BF_DEV'
 
-SNOWFLAKE_ROLE = 'BF_DEVELOPER1007'
-SNOWFLAKE_WAREHOUSE = 'BF_ETL1007'
-SNOWFLAKE_STAGE = 's3_stage_trans_order'
-
-S3_FILE_PATH_TEMPLATE = 's3://octde2024/airflow_project/Transaction_Team3_{{ ds_nodash }}.csv'
 
 
 with DAG(
