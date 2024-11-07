342c41861c35edda9b99a7e9637ec7d3ab62cc98
Bo-Yu-Columbia <by2345@columbia.edu>
2024-11-07 16:41:46 -0500

    task 1 testcase update

--- dags/s3_to_snowflake_team2.py
+++ dags/s3_to_snowflake_team2.py
@@ -10,13 +10,36 @@ SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER'
 
 with DAG(
     's3_to_snowflake_team2',
-    start_date=datetime(2024, 11, 7),
-    end_date=datetime(2024, 11, 7),
+    start_date=datetime(2024, 11, 6),
+    end_date=datetime(2024, 11, 8),
     schedule_interval='0 8 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire', 'team2'],
     catchup=True,
 ) as dag:
+    create_table = SnowflakeOperator(
+        task_id='create_snowflake_table',
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        sql=
+        '''
+        CREATE TABLE IF NOT EXISTS AIRFLOW1007.BF_DEV.prestage_AirQuality_Team2 (
+        Date DATE,
+        Time TIME,
+        "CO(GT)" FLOAT,
+        "PT08.S1(CO)" INT,
+        "NMHC(GT)" INT,
+        "C6H6(GT)" FLOAT,
+        "PT08.S2(NMHC)" INT,
+        "NOx(GT)" INT,
+        "PT08.S3(NOx)" INT,
+        "NO2(GT)" INT,
+        "PT08.S4(NO2)" INT,
+        "PT08.S5(O3)" INT,
+        T FLOAT,
+        RH FLOAT,
+        AH FLOAT)
+        '''
+    )
 
     copy_into_prestg = CopyFromExternalStageToSnowflakeOperator(
         task_id='copy_csv_into_snowflake',
@@ -29,4 +52,4 @@ with DAG(
         file_format='csv_format_team2',
     )
 
-    copy_into_prestg
+    create_table >> copy_into_prestg
