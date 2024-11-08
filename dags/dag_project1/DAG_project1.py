3a97e69fca032c22d19fdc70538840b83067e8b2
mag1cfrog <harrywong2017@gmail.com>
2024-11-08 06:57:07 -0800

    Update end date for Team3 incremental DAG to extend schedule

--- dags/dag_project1/DAG_project1.py
+++ dags/dag_project1/DAG_project1.py
@@ -13,7 +13,7 @@ from snowflake_tasks import create_prestage_table, load_data_to_snowflake
 with DAG(
     "Team3_s3_to_s_incremental_DAG",
     start_date=datetime(2024, 11, 6),
-    end_date = datetime(2024, 11, 8),
+    end_date = datetime(2024, 11, 9),
     schedule_interval='0 1 * * *',# UTC timezone, everyday at 1am
     catchup=True,
     tags=['Team3']
