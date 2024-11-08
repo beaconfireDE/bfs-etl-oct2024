af31aebb0a9af0a0d3c7fa654e7517f14cd193fd
Jiarui Tang <jiarui_tang@outlook.com>
2024-11-07 22:17:24 -0500

    Change time to retest

--- dags/project1_s3_to_snowflake.py
+++ dags/project1_s3_to_snowflake.py
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS {SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.{PRESTAGE_TAB
 with DAG(
     "project1_s3_to_snowflake_team1",
     start_date=datetime(2024, 11, 5),
-    end_date=datetime(2024, 11, 8),
-    schedule_interval='04 3 * * *',
+    end_date=datetime(2024, 11, 6),
+    schedule_interval='20 3 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
     catchup=True,
